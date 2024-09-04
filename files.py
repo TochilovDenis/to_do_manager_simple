6719181629aef9e8d34d685d61881dd7f4cf9b03
DEX <denis-tochilov@yandex.ru>
2024-09-04 15:30:12 +0300

    Оптимизируем скрипт для генерации и записи чисел в файл.txt Улучшает эффективность скрипта, используя списковое включение для создания форматированных чисел вместо цикла и аппендинга.
    1. Заменяется явный цикл и операции аппендинга на компрехенсию списков
    
    Оптимизированный скрипт теперь создает список 49 форматированных чисел (с 1 до 49) за одну строку и сразу записывает их в 'file.txt' без промежуточных шагов.

--- files.py
+++ files.py
@@ -1,7 +1,11 @@
-generate_num: list = []
+# Создаем список форматированных строк чисел от 1 до 49
+# Используем списковое включение для компактного создания списка
+# где внутри [этого кода это компрехенсия список]
+generate_num: list = [str(i) + '\n' for i in range(1, 50)]
 
-for i in range(50):
-    generate_num.append(str(i) + '\n')
+# generate_num: list = []
+# for i in range(50):
+#     generate_num.append(str(i) + '\n')
 
 
 file = open('file.txt', 'w')
