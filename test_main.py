6b655b530185239ac876fec922212ae60f6451d6
DEX <denis-tochilov@yandex.ru>
2024-09-04 11:56:02 +0300

    Добавлена проверка вывода непустого списка задач
    - Реализован тест print_list_of_task для случая непустого списка задач
    - Проверяется корректное отображение задачи в формате [индекс]: название

--- test_main.py
+++ test_main.py
@@ -5,3 +5,11 @@ def test_print_list_of_task_empty(capsys):
     print_list_of_task(tasks)
     captured = capsys.readouterr()
     assert captured.out.strip() == "Список задач пуст"
+
+
+def test_print_list_of_task_not_empty(capsys):
+    tasks = []
+    tasks.append("task-01")
+    print_list_of_task(tasks)
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "[0]: task-01"
