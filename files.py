3e3507733347ee4b3474e4ae3bee49150b94b005
DEX <denis-tochilov@yandex.ru>
2024-09-06 21:24:34 +0300

    Добавлена функция save_tasks_file для записи списка задач в текстовый файл.
    Основные изменения:
    
    - Создана функция save_tasks_file, которая принимает список строковых задач и имя файла для сохранения.
    - Использован контекстный менеджер with open() для безопасного открытия и закрытия файла.
    - Содержимое списка задач записывается в файл как одна строка, разделенная символами новой строки.

--- files.py
+++ files.py
@@ -1,14 +1,3 @@
-FILENAME = 'file.txt'
-
-def generate_list() -> list[str]:
-    """
-    Функция для создания списка строк из целочисленного диапазона от 0 до 50
-    :return: a list of strings [компрехенсия список]
-    """
-    return [str(i) + '\n' for i in range(1, 50)]
-
-
-
 def write_to_file(list_to_write: list[str], filename: str) -> None:
     """
     Функция для записи списка строк в файл с именем filename
@@ -29,24 +18,13 @@ def read_from_file(filename: str) -> list[str]:
         result = file.readlines()
     return result
 
-def print_list(list_to_print: list[str]) -> None:
+
+def save_tasks_file(list_of_tasks: list[str],filename:str ) -> None:
     """
-    Функция для печати списка строк
-    :param list_to_print: список для печати
+    Функция для записи списка задач в файл с именем filename
+    :param list_of_tasks:  список задачи для записи в файл
+    :param filename: в какой файл записывать список
     :return: nothing
     """
-    for el in list_to_print:
-        print(el, end='')
-
-def main() -> None:
-    """
-    Основная функция
-    :return: nothing
-    """
-    l: list[str] = generate_list()
-    write_to_file(l, FILENAME)
-    new_list = read_from_file(FILENAME)
-    print_list(new_list)
-
-if __name__ == '__main__':
-    main()
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(list_of_tasks))
