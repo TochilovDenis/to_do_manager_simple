84d8db3be15ee539bd5a707feefbad8025238b0b
DEX <denis-tochilov@yandex.ru>
2024-09-15 16:14:06 +0300

    Добавлено описание документации для проверки дат

--- date_parser.py
+++ date_parser.py
@@ -1,7 +1,13 @@
 """ Модуль для функций разбора дат """
+
 from datetime import datetime
 
 def is_date(date: str) -> bool:
+    """
+    Функция для проверки того, является ли строка датой в соответствии с форматом
+    :param date: строка, представляющая дату
+    :return: True, если строка является датой в соответствии с форматом, else False
+    """
     template = '%Y-%m-%d'
     try:
         datetime.strptime(date, template)
