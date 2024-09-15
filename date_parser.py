531b3cebf6a8ccc394e97ca06f387e8c0e03eaac
DEX <denis-tochilov@yandex.ru>
2024-09-15 16:32:07 +0300

    Обновление функции is_date в функцию is_date_by_format и принимает формат на вход. Также создан список FORMATS в котором будем хранить все возможные форматы дат.  - у нас их два: с дефисом(-) и с наклонной чертой(/).

--- date_parser.py
+++ date_parser.py
@@ -2,15 +2,23 @@
 
 from datetime import datetime
 
-def is_date(date: str) -> bool:
+FORMATS: list = ["%Y-%m-%d", "%Y/%m/%d"]
+
+def is_date_by_format(date: str, format_date: str) -> bool:
     """
     Функция для проверки того, является ли строка датой в соответствии с форматом
     :param date: строка, представляющая дату
     :return: True, если строка является датой в соответствии с форматом, else False
     """
-    template = '%Y-%m-%d'
     try:
-        datetime.strptime(date, template)
+        datetime.strptime(date, format_date)
         return True
     except ValueError:
         return False
+
+
+def is_date(date: str) -> bool:
+    for format_date in FORMATS:
+        if is_date_by_format(date, format_date):
+            return True
+    return False
